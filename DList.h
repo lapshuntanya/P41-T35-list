60675aab21dfa410d2d534eafd2f84587f46f58c
lapshuntanya <lapshun.tatiana@gmail.com>
2025-06-03 17:42:59 +0300

    DList

--- DList.h
+++ DList.h
@@ -13,10 +13,52 @@ namespace MyList {
 		DNode* prev;
 
 		DNode(T value) { info = value; next = prev =  nullptr; }
-		void printNode()const {
+		void printDNode()const {
 			cout << "INFO: " << info << " SELF: " << this
 				<< "\n\t PREV: " << prev 
 				<< "\n\t NEXT: " << next << endl;
+		}
+	};
+
+	template <typename U>
+	class DList {
+		DNode<U>* head;
+		DNode<U>* tail;
+	public:
+		DList() {
+			head = tail = nullptr;
+		}
+		~DList() {
+			//empty
+		}
+		void push_back(U value) {
+			// 1- Створюємо вузол
+			DNode<U>* el = new DNode<U>(value);
+
+			//2 - Немає вузлів в списку
+			if (head == nullptr) {
+				head = tail = el;
+			}
+			else {
+				//3-1 Прив'язуємо вузли
+				tail->next = el;
+				el->prev = tail;
+
+				//3-2 Зсовуємо хвіст в кінець
+				tail = el;
+			}
+		}
+
+		void showList() {
+			DNode<U>* p = head;
+
+			if (p == nullptr) cout << "List is empty!\n";
+			else {
+				while (p != nullptr) {
+					p->printDNode();
+					p = p->next;
+				}
+			}
 		}
 	};
 }
